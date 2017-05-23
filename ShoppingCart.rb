0d4d5c174c92f6d2de82cd0fdb00cd416b6a400c
Roopesh <rrattan.89@gmail.com>
2017-05-23 16:24:12 -0400

    Shopping Cart edited

--- ShoppingCart.rb
+++ ShoppingCart.rb
@@ -14,30 +14,23 @@ class ShoppingCart
     @@Products
   end
 
-  def sum
-    total_cost.each do |product|
-    @cost_before_tax = products
-    @cost_after_tax = products.base_price * tax_rate
-    end
-  end
-
-  def add_product(name, base_price, tax_rate)
+  def self.add_product(name, base_price, tax_rate)
     new_product = Product.new(name, base_price, tax_rate)
     @@Products << new_product
   end
 
-  def remove_product(name)
+  def self.remove_product(name)
     @@Products.delete(name)
   end
 
-  def sum_before_tax
+  def self.sum_before_tax
     @@Products.each do |product|
       @sum_before_tax += product.base_price
     end
     @sum_before_tax = @sum_before_tax.round(2)
   end
 
-  def sum_after_tax
+  def self.sum_after_tax
     @@Products.each do |product|
       @sum_after_tax += product.total_price
     end
